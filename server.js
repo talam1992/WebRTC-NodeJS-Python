42a862198157305705b5035eca53b7250354db07
Timothy Lam <timothylam92@hotmail.co.uk>
2019-05-02 17:20:13 +0100

    Basic Connection
    WebRTC

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-console.log('server started on port ');
-
-app.get('/', function(req, res){
-	res.render('index.ejs');
-});
-
-app.listen(3000);
